e3536f23c597d2a2007d4e6e79a53c529281964a
L1ndseyHerman <lherman21@jcu.edu>
2021-09-14 17:59:33 -0400

    Caught up to vid204: handling form input validation the best way.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,68 @@
+import {useState} from 'react';
+
 const SimpleInput = (props) => {
+
+  //  Could update this with every keystroke, or at the end with a useRef.
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== '';
+  //  Another less-code boolean:
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  let formIsValid = false;
+
+  //  If u had other form inputs, should do for enteredAgeIsValid or whatevs in a useEffect().
+    if (enteredNameIsValid) {
+      formIsValid = true;
+    } else {
+      formIsValid = false;
+    }
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    setEnteredNameTouched(true);
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    console.log(enteredName);
+
+    //  This is one place where changing the state every keystroke is better,
+    //  bec shouldn't really modify a ref, just read it.
+    setEnteredName('');
+    setEnteredNameTouched(false);
+    //  Ng :( Vanilla JS code that manipulates the DOM, when React should manipulate the DOM.
+    //nameInputRef.current.value = '';
+  };
+
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input 
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler} 
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
