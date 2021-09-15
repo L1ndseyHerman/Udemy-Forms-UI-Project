7b1b85631d327d732c3980b510315b3e1e180ea5
L1ndseyHerman <lherman21@jcu.edu>
2021-09-14 23:10:41 -0400

    Done with Section 16, more useReducer().

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,31 +1,50 @@
-import {useState} from 'react';
+import {useReducer} from 'react';
+
+const initialInputState = {
+    value: '',
+    isTouched: false
+};
+
+const inputStateReducer = (state, action) => {
+
+    if (action.type === 'INPUT') {
+        return {value: action.value, isTouched: state.isTouched};
+    }
+    if (action.type === 'BLUR') {
+        //  Apparently, the ordering of params doesn't matter?
+        return {isTouched: true, value: state.value};
+    }
+    if (action.type === 'RESET') {
+        return {isTouched: false, value: ''};
+    }
+    return inputStateReducer;
+};
 
 //  The function passed in will decide if valueIsValid is t/f.
 const useInput = (validateValue) => {
-    const [enteredValue, setEnteredValue] = useState('');
-    const [isTouched, setIsTouched] = useState(false);
+
+    const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
 
     //  This calls the function from the other component on enteredValue.
-    const valueIsValid = validateValue(enteredValue);
-    const hasError = !valueIsValid && isTouched;
+    const valueIsValid = validateValue(inputState.value);
+    const hasError = !valueIsValid && inputState.isTouched;
 
     const valueChangeHandler = event => {
-        setEnteredValue(event.target.value);
+        dispatch({type: 'INPUT', value: event.target.value});
       };
 
     const inputBlurHandler = event => {
-        setIsTouched(true);
+        dispatch({type: 'BLUR'});
       };
 
     const reset = () => {
-        setEnteredValue('');
-        setIsTouched(false);
+        dispatch({type: 'RESET'});
     };
 
     //  hasError: hasError,
     //  But why aren't the functions set to a property?
     return {
-        value: enteredValue, 
+        value: inputState.value, 
         isValid: valueIsValid,
         hasError, 
         valueChangeHandler, 
