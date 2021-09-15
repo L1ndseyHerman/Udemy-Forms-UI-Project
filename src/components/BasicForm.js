99bce3032b3e06cac491b0fb2e4a1b6d87581df2
L1ndseyHerman <lherman21@jcu.edu>
2021-09-14 22:51:10 -0400

    Caught up to vid209. I mostly kept my basic form code (my variable names), but also added his isNotEmpty bec I didn't think to avoid duplicate function code that way.

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,101 @@
+import useInput from "../hooks/use-input";
+
+const isNotEmpty = value => value.trim() !== '';
+const isEmail = value => value.includes('@');
+
 const BasicForm = (props) => {
+
+  //  An alias for value:
+  const {
+    value: enteredFirstName,
+    isValid: enteredFirstNameIsValid, 
+    hasError: firstNameInputHasError, 
+    valueChangeHandler: firstNameChangedHandler, 
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstNameInput
+    //  This function gets executed in use-input. (validateValue(enteredValue)).
+  } = useInput(isNotEmpty);
+
+    const {
+      value: enteredLastName,
+      isValid: enteredLastNameIsValid, 
+      hasError: lastNameInputHasError, 
+      valueChangeHandler: lastNameChangedHandler, 
+      inputBlurHandler: lastNameBlurHandler,
+      reset: resetLastNameInput
+    } = useInput(isNotEmpty);
+
+    const {
+      value: enteredEmail,
+      isValid: enteredEmailIsValid, 
+      hasError: emailInputHasError, 
+      valueChangeHandler: emailChangedHandler, 
+      inputBlurHandler: emailBlurHandler,
+      reset: resetEmailInput
+    } = useInput(isEmail);  
+
+    let formIsValid = false;
+
+    if (enteredFirstNameIsValid && enteredLastNameIsValid && enteredEmailIsValid)
+    {
+      formIsValid = true;
+    }
+
+    const formSubmissionHandler = event => {
+      event.preventDefault();
+  
+      if (!formIsValid) {
+        return;
+      }
+  
+      resetFirstNameInput();
+      resetLastNameInput();
+      resetEmailInput();
+    };
+  
+    const firstNameInputClasses = firstNameInputHasError ? 'form-control invalid' : 'form-control';
+    const lastNameInputClasses = lastNameInputHasError ? 'form-control invalid' : 'form-control';
+    const emailInputClasses = emailInputHasError ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+        <div className={firstNameInputClasses}>
+          <label htmlFor='firstName'>First Name</label>
+          <input
+           type='text' 
+           id='firstName'
+           onChange={firstNameChangedHandler} 
+           onBlur={firstNameBlurHandler}
+           value={enteredFirstName}
+          />
+          {firstNameInputHasError && <p className="error-text">Please enter a first name.</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className={lastNameInputClasses}>
+          <label htmlFor='lastName'>Last Name</label>
+          <input
+           type='text' 
+           id='lastName' 
+           onChange={lastNameChangedHandler} 
+           onBlur={lastNameBlurHandler}
+           value={enteredLastName}
+          />
+          {lastNameInputHasError && <p className="error-text">Please enter a last name.</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>E-Mail Address</label>
+        <input 
+          type='email' 
+          id='email' 
+          onChange={emailChangedHandler} 
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+        />
+        {emailInputHasError && <p className="error-text">Please enter a valid email.</p>}
       </div>
       <div className='form-actions'>
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
